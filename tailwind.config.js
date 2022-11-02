d120542cb4a9546dee55c49479ed87ec4bde872b
OlhaBaturina <olha.baturina@gmail.com>
2022-11-02 13:16:58 +0000

    Add base style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: [
-    './public/**/*.{html,js}',
-    './components/**/*.{html,js}',
-  ],
-  darkMode: false,
   content: [
     "./src/**/*.{html,js}",
     "./*.{html,js}"
@@ -14,17 +9,21 @@ module.exports = {
     colors: {
       primary: '#151515',
       accent: '#F6B600',
-      accentHover: '#F6B4009A',
+      accentHover: '#F9CA49',
+      textWhite: '#FFFFFE'
 
     },
     fontFamily: {
-      heading: ['Oswald', 'sans-serif'],
-      body: ['Open Sans', 'sans-serif'],
+      title: ['Oswald', 'sans-serif'],
+      body: ['"Open Sans"', 'sans-serif'],
     },
     extend: {
       // задати значення
       spacing: {
-        '8xl': '96rem',
+        's': '2rem',
+        'm': '3rem',
+        'l': '4rem',
+        'xl': '5rem',
         '9xl': '128rem',
       },
       borderRadius: {
@@ -32,8 +31,5 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    plugin(function({ addUtilities, addComponents, e, config }) {
-      // Add your custom styles here
-    })],
+  plugins: [],
 }
