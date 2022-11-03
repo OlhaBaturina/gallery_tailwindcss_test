b313f5330cee8bd4bf721a73cff41224c0ff807f
OlhaBaturina <olha.baturina@gmail.com>
2022-11-03 11:16:43 +0000

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   mode: 'jit',
   content: [
     "./src/**/*.{html,js}",
     "./*.{html,js}"
   ],
+
   theme: {
     colors: {
       primary: '#151515',
       accent: '#F6B600',
       accentHover: '#CFA226',
       textWhite: '#FFFFFE'
-
     },
+
     fontFamily: {
       title: ['Oswald', 'sans-serif'],
       body: ['"Open Sans"', 'sans-serif'],
     },
+    
     screens: {
       'tablet': '576px',
       'desktop': '960px',
     },
     extend: {
-      // задати значення
       spacing: {
         'xs': '32px',
         's': '2rem',
@@ -30,15 +32,7 @@ module.exports = {
         'l': '4rem',
         'xl': '5rem',
         '2xl': '6rem',
-        '3xl': '7rem',
-
       },
-      borderRadius: {
-        '4xl': '2rem',
-      }
     },
   },
-  plugins: [
-    
-  ],
 }
